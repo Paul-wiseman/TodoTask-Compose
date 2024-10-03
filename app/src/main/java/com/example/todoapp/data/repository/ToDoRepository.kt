5c06a10a537dd169c278804fb8c50d7b363462cf
Paul Ndibe <paulndibe92@gmail.com>
2024-10-03 08:17:33 +0100

    created database and database dao

---
+++ app/src/main/java/com/example/todoapp/data/repository/ToDoRepository.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp.data.repository
+
+import com.example.todoapp.data.ToDoDao
+import com.example.todoapp.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(val toDoDao: ToDoDao) {
+
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTask()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByLHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> = toDoDao.getSelectTask(taskId)
+
+    suspend fun addTask(todoTask: ToDoTask) = toDoDao.addTask(todoTask)
+
+    suspend fun updateTask(todoTask:ToDoTask) = toDoDao.updateTask(todoTask)
+
+    suspend fun deleteTask(todoTask:ToDoTask) = toDoDao.deleteTask(todoTask)
+
+    suspend fun deleteAllTask() = toDoDao.deleteAllTasks()
+
+    fun searchDatabase(searchQuery:String) = toDoDao.searchDatabase(searchQuery)
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+import org.jetbrains.kotlin.fir.declarations.builder.buildScript
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    alias(libs.plugins.hilt) apply false
+}
